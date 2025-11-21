807b28b25fe3357b94c50e9506ddef00604e3022
FabrizioPLC <fabrizio0943@gmail.com>
2025-11-20 23:53:20 -0500

    envio tokens a wallet 2.0

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Req } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { userDto } from './dto/userDto';
 import { UsersEntity } from './users.entity';
@@ -8,44 +8,48 @@ import { JwtAuthGuard } from './auth/jwt-auth.guard';
 @UseGuards(JwtAuthGuard)
 @Controller('users')
 export class UsersController {
-    constructor(private readonly usersService: UsersService) { }
+    constructor(private readonly usersService: UsersService){}
 
     @Get()
-    getUsers(): Promise<UsersEntity[] | null> {
+    getUsers(): Promise<UsersEntity[] | null>{
         return this.usersService.getAllUsers();
     }
 
     @Get(':user_id')
-    getUserById(@Param('user_id') id: number): Promise<UsersEntity | null> {
+    getUserById(@Param('user_id') id: number): Promise<UsersEntity | null>{
         return this.usersService.getUser(id);
     }
 
     @Get('phone/:phone')
-    getUserByPhone(@Param('phone') phone: string): Promise<UsersEntity | null> {
+    getUserByPhone(@Param('phone') phone: string): Promise<UsersEntity | null>{
         return this.usersService.getUserByPhone(phone);
     }
 
+    @Get('dni/:dni')
+    async getUserByDni(@Param('dni') dni: string): Promise<UsersEntity | null>{
+        return this.usersService.getUserByDni(dni);
+    }
+
+
     @Get('verify/:phone')
-    async verifyUserByPhone(@Param('phone') phone: string): Promise<boolean> {
+    async verifyUserByPhone(@Param('phone') phone: string): Promise<boolean>{
         const user = await this.usersService.getUserByPhone(phone);
         console.log(user);
         if (!user) return false;
         return true;
     }
 
-    @Get('dni/:dni')
-    async getUserByDni(@Param('dni') dni: string): Promise<UsersEntity | null> {
-        return this.usersService.getUserByDni(dni);
-    }
-
     @Post()
-    async createUser(@Body() userDto: userDto) {
-        await this.usersService.createUser(userDto);
+    async createUser(@Body() userDto: userDto, @Req() req: any) {
+    
+    const token = req.headers.authorization?.replace("Bearer ", "");
+
+    await this.usersService.createUser(userDto, token);
     }
 
-
+    
     @Patch(':user_id')
-    async updateUser(@Param('user_id') user_id: number, @Body() body: any) {
+    async updateUser(@Param('user_id') user_id: number, @Body() body: any){
         await this.usersService.updateUser(body, user_id)
     }
 
