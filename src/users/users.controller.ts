81b9ade714cbb1a57b645bd614ae915914f736cb
FabrizioPLC <fabrizio0943@gmail.com>
2025-11-22 08:53:42 -0500

    guards por metodo

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,33 +6,37 @@ import { UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { OrGuard } from './auth/or.guard';
 
-@UseGuards(JwtAuthGuard)
+
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService){}
 
+    @UseGuards(JwtAuthGuard)
     @Get()
     getUsers(): Promise<UsersEntity[] | null>{
         return this.usersService.getAllUsers();
     }
 
+    @UseGuards(JwtAuthGuard)
     @Get(':user_id')
     getUserById(@Param('user_id') id: number): Promise<UsersEntity | null>{
         return this.usersService.getUser(id);
     }
 
-    @Get('phone/:phone')
+
     @UseGuards(OrGuard)
+    @Get('phone/:phone')
     getUserByPhone(@Param('phone') phone: string): Promise<UsersEntity | null>{
         return this.usersService.getUserByPhone(phone);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Get('dni/:dni')
     async getUserByDni(@Param('dni') dni: string): Promise<UsersEntity | null>{
         return this.usersService.getUserByDni(dni);
     }
 
-
+    @UseGuards(JwtAuthGuard)
     @Get('verify/:phone')
     async verifyUserByPhone(@Param('phone') phone: string): Promise<boolean>{
         const user = await this.usersService.getUserByPhone(phone);
@@ -41,6 +45,7 @@ export class UsersController {
         return true;
     }
 
+    @UseGuards(JwtAuthGuard)
     @Post()
     async createUser(@Body() userDto: userDto, @Req() req: any) {
     
@@ -49,7 +54,7 @@ export class UsersController {
     await this.usersService.createUser(userDto, token);
     }
 
-    
+    @UseGuards(JwtAuthGuard)
     @Patch(':user_id')
     async updateUser(@Param('user_id') user_id: number, @Body() body: any){
         await this.usersService.updateUser(body, user_id)
